25ed02d98cbf010211bc0b0b1c847011475bde41
Presley <presleygs.1607@gmail.com>
2023-08-14 23:49:20 -0300

    initial

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,7 @@
 import { parse as csvParse } from 'csv-parse';
 import fs from 'fs';
 import { ICategoriesRepository } from '../../model/repositories/ICategoriesRepository';
+import { Category } from '../../model/Category';
 
 interface IImportCategory {
 	name: string;
@@ -9,26 +10,46 @@ interface IImportCategory {
 class ImportCategoryUseCase {
 	constructor(private categoriesRepository: ICategoriesRepository) {}
 
-	loadCategories(file: Express.Multer.File): IImportCategory[] {
-		const stream = fs.createReadStream(file.path);
-		const categories: IImportCategory[] = [];
+	loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+		return new Promise((resolve, reject) => {
+			const stream = fs.createReadStream(file.path);
+			const categories: IImportCategory[] = [];
 
-		const parseFile = csvParse();
+			const parseFile = csvParse();
 
-		stream.pipe(parseFile);
+			stream.pipe(parseFile);
 
-		parseFile.on('data', async (line) => {
-			const [name, description] = line;
-			categories.push({
-				name,
-				description,
-			});
-			return categories;
+			parseFile
+				.on('data', async (line) => {
+					const [name, description] = line;
+					categories.push({
+						name,
+						description,
+					});
+				})
+				.on('end', () => {
+					resolve(categories);
+				})
+				.on('error', (err) => {
+					reject(err);
+				});
 		});
 	}
-	execute(file: Express.Multer.File): void {
-		const categories = this.loadCategories(file);
-		console.log(categories);
+	async execute(file: Express.Multer.File): Promise<void> {
+		const categories = await this.loadCategories(file);
+
+		categories.map(async (category) => {
+			const { name, description } = category;
+
+			const existCategory = this.categoriesRepository.findByName(name);
+
+			if (!existCategory) {
+				this.categoriesRepository.create({
+					name,
+					description,
+				});
+			}
+		});
 	}
 }
 export { ImportCategoryUseCase };
